4d892450b8cc4bb11865c1af28d992cf50067640
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-10-09 08:35:31 +0200

    docs: document schema

--- packages/starlight-sidebar-topics/libs/config.ts
+++ packages/starlight-sidebar-topics/libs/config.ts
@@ -7,23 +7,46 @@ const sidebarTopicBadgeSchema = z.object({
 })
 
 const sidebarTopicBaseSchema = z.object({
-  // TODO(HiDeoo) comment
+  /**
+   * An optional badge to display next to the topic label.
+   *
+   * This option accepts the same configuration as the Starlight badge sidebar item configuration.
+   * @see https://starlight.astro.build/guides/sidebar/#badges
+   */
   badge: z
     .union([z.string(), sidebarTopicBadgeSchema])
     .transform((badge) => (typeof badge === 'string' ? { variant: 'default' as const, text: badge } : badge))
     .optional(),
-  // TODO(HiDeoo) comment
+  /**
+   * The name of an optional icon to display before the topic label set to one of Starlight’s built-in icons.
+   * @see https://starlight.astro.build/reference/icons/#all-icons
+   */
   icon: z.string().optional(),
-  // TODO(HiDeoo) comment
+  /**
+   * The topic label visible at the top of the sidebar.
+   *
+   * The value can be a string, or for multilingual sites, an object with values for each different locale. When using
+   * the object form, the keys must be BCP-47 tags (e.g. en, fr, or zh-CN).
+   */
   label: z.union([z.string(), z.record(z.string())]),
-  // TODO(HiDeoo) comment
+  /**
+   * The link to the topic’s content which an be a relative link to local files or the full URL of an external page.
+   *
+   * For internal links, the link can either be a page included in the items array or a different page acting as the
+   * topic’s landing page.
+   */
   link: z.string(),
 })
 
 const sidebarTopicLinkSchema = sidebarTopicBaseSchema
 
 const sidebarTopicGroupSchema = sidebarTopicBaseSchema.extend({
-  // TODO(HiDeoo) comment
+  /**
+   * The sidebar items (links and subcategories) to display for this topic.
+   *
+   * The topic’s sidebar navigation items. This represents the sidebar displayed when the topic `link` page or any of
+   * the pages configured in the `items` array is the current page.
+   */
   items: z.any().array() as z.Schema<NonNullable<StarlightUserConfig['sidebar']>>,
 })
 
