fbd78f3320f98c4cfe9d1d7c900f31a7ff73f866
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-12-17 16:30:54 +0100

    feat: add support for astro 5

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,7 +1,8 @@
+import { docsLoader } from '@astrojs/starlight/loaders'
 import { docsSchema } from '@astrojs/starlight/schema'
 import { defineCollection } from 'astro:content'
 import { topicSchema } from 'starlight-sidebar-topics/schema'
 
 export const collections = {
-  docs: defineCollection({ schema: docsSchema({ extend: topicSchema }) }),
+  docs: defineCollection({ loader: docsLoader(), schema: docsSchema({ extend: topicSchema }) }),
 }
