91a8ba236e5dbd78719acddcb55c66c159133e23
Andreas Deininger <andreas@deininger.net>
2026-05-28 14:26:57 +0200

    docs: fix typos

--- packages/starlight-sidebar-topics/libs/plugin.ts
+++ packages/starlight-sidebar-topics/libs/plugin.ts
@@ -4,6 +4,6 @@ export function throwPluginError(message: string, hint?: string): never {
   throw new AstroError(
     message,
     hint ??
-      `See the error report above for more informations.\n\nIf you believe this is a bug, please file an issue at https://github.com/HiDeoo/starlight-sidebar-topics/issues/new/choose`,
+      `See the error report above for more information.\n\nIf you believe this is a bug, please file an issue at https://github.com/HiDeoo/starlight-sidebar-topics/issues/new/choose`,
   )
 }
